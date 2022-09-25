c596067a9fefb2f56d15a7415c2adaca5e57532f
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-09-24 19:24:33 -0700

    Updated explore section with subheader and imgs

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import './Explore.css'
 
+import explore1 from '../../assets/images/explore-1.jpg'
+import explore2 from '../../assets/images/explore-2.jpg'
+import explore3 from '../../assets/images/explore-3.jpg'
+
 const Explore = () => {
   return (
-    <div className='container'>
-        <div>
+    <div className='explore'>
             <h1>Explore cabins that are available near your area.</h1>
-        </div>
+            <div className='sub-header'>
+                <p><span className='bold'>All</span></p>
+                <p>Forests</p>
+                <p>Mountains</p>
+                <p>Woodlands</p>
+                <p>Residential</p>
+            </div>
+            <div className='image-grid'>
+                <img src={explore1} alt='' />
+                <img src={explore2} alt='' />
+                <img src={explore3} alt='' />
+            </div>
+            <button className='btn'>View All</button>
     </div>
   )
 }
