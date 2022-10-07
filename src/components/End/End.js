7286c7ae458567a6424d174b9eff18c8c1baec00
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-10-06 17:30:35 -0700

    Stylized the end elements with flexbox

--- src/components/End/End.js
+++ src/components/End/End.js
@@ -5,30 +5,36 @@ const End = () => {
   return (
     <div className='end'>
         <div className='container'>
-            <h3 className='our-brand'>Our Brand</h3>
-                <ul className='our-brand-list'>
-                    <li>About Us</li>
-                    <li>Terms and Conditions</li>
-                    <li>Privacy Policy</li>
-                    <li>Sustainability</li>
-                    <li>Terms of Service</li>
-                </ul>
-            <h3 className='assistance'>Assistance</h3>
-                <ul className='assistance-list'>
-                    <li>Customer Service</li>
-                    <li>FAQs</li>
-                    <li>Partner Help</li>
-                    <li>Investor Relations</li>
-                    <li>Partner Dispute</li>
-                    <li>Don't sell my personal information</li>
-                    <li>Corporate Contact</li>
-                </ul>
-            <h3 className='newsletter'>Newsletter</h3>
-                <p>Subscribe to recieve updates, access to exclusive real estate proposals, and more.</p>
-                <form>
-                    <input type='email' placeholder='Enter your email address'></input>
-                    <button type='button' name='button'>SUBSCRIBE</button>
-                </form>
+            <div className='our-brand list'>
+                <h3 className='our-brand-header'>Our Brand</h3>
+                    <ul className='our-brand-list'>
+                        <li>About Us</li>
+                        <li>Terms and Conditions</li>
+                        <li>Privacy Policy</li>
+                        <li>Sustainability</li>
+                        <li>Terms of Service</li>
+                    </ul>
+            </div>
+            <div className='assistance list'>
+                <h3 className='assistance-header'>Assistance</h3>
+                    <ul className='assistance-list'>
+                        <li>Customer Service</li>
+                        <li>FAQs</li>
+                        <li>Partner Help</li>
+                        <li>Investor Relations</li>
+                        <li>Partner Dispute</li>
+                        <li>Don't sell my personal information</li>
+                        <li>Corporate Contact</li>
+                    </ul>
+            </div>
+            <div className='newsletter list'>
+                <h3 className='newsletter-header'>Newsletter</h3>
+                    <p>Subscribe to recieve updates, access to exclusive real estate proposals, and more.</p>
+                    <form>
+                        <input type='email' placeholder='Enter your email address'></input>
+                        <button type='button' name='button'>SUBSCRIBE</button>
+                    </form>
+            </div>
         </div>
     </div>
   )
