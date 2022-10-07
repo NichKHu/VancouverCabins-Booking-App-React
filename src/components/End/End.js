e594130fdd511e77c059ee93cb5fbff144ca6de1
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-10-07 01:07:59 -0700

    Added info on ending components, updated btn, adjusted margins for other components

--- src/components/End/End.js
+++ src/components/End/End.js
@@ -23,18 +23,28 @@ const End = () => {
                         <li><a href="#">Partner Help</a></li>
                         <li><a href="#">Investor Relations</a></li>
                         <li><a href="#">Partner Dispute</a></li>
-                        <li><a href="#">Don't sell my personal information</a></li>
+                        <li><a href="#">Personal Information Query</a></li>
                         <li><a href="#">Corporate Contact</a></li>
                     </ul>
             </div>
             <div className='newsletter list'>
                 <h3 className='header'>Newsletter</h3>
-                    <p>Subscribe to recieve updates, access to exclusive real estate proposals, and more.</p>
+                    <p className='newsletter-info'>Subscribe to recieve updates, access to exclusive real estate proposals, and more.</p>
                     <form>
                         <input className='user-input' type='email' placeholder='Enter your email address'></input>
                         <button className='btn-subscribe' type='button' name='button'>Subscribe</button>
                     </form>
             </div>
+            <div className='contact list'>
+                <h3 className='contact-us'>Contact Us</h3>
+                    <p className='supp-email'>support@vancouvercabins.com</p>
+                    <h4>Call Us</h4>
+                    <p>M-F | 8am-6pm PT</p>
+                    <p className='phone-num'>+44 20 3663 0230</p>
+            </div>
+        </div>
+        <div className='copyright-ending'>
+                <p>© 2022 VancouverCabins Ltd-All rights reserved GB VAT 219 6348 89 - 307 Linen Hall 162-168 Regent Street W1B 5TE</p>
         </div>
     </div>
   )
